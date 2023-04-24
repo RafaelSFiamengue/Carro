9b47ceb9ff8f431dad7013726dfa44f8b6b542f0
RafaelSFiamengue <127798981+RafaelSFiamengue@users.noreply.github.com>
2023-04-24 20:19:59 -0300

    Update Carro.cpp

--- Carro.cpp
+++ Carro.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
 using namespace std;
-
+//Criação da classe carro
 class Carro
 {
     public:
-    
+    //Base de como o carro será construido, suas especificidades
     string cor;
     string modelo;
     float velmax;
     void Buzinar();
 };
-
+//Programa Principal
 int main()
 {
+    //Objeto meucarro sendo criado a partir da classe carro
     Carro MeuCarro;
-    
+    //Especificidades do objeto meu carro
     MeuCarro.cor = "Azul";
     MeuCarro.modelo = "MCLAREN";
     MeuCarro.velmax = 220.0f;
-    
+    //Imprime tudo sobre o meucarro para o usuário
     MeuCarro.Buzinar();
     cout << MeuCarro.cor << endl;
     cout << MeuCarro.modelo << endl;
     cout << MeuCarro.velmax << endl;
 }
-
+//Método da buzina e como é feita para o usuario
 void Carro::Buzinar(){
     cout << "Bi,Bi!!" << endl; 
 }
